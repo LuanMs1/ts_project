ac45d21da9c0e70662d243c5f27a345ab7ef900a
Luan <luan.mesquita.sobrinho@gmail.com>
2022-11-15 19:05:28 -0300

    starting ORM
    Co-authored-by: Nayara Dalla Vecchia <nayandrade@users.noreply.github.com>

--- src/repositories/ORM/index.ts
+++ src/repositories/ORM/index.ts
@@ -1,6 +1,18 @@
 // Arquivo para funções gerais de acesso ao PostegreSQL
-import { Pool } from "pg";
-const poolConfig = {};
+import { Pool, PoolConfig } from "pg";
+// import * as dotenv from 'dotenv';
+// dotenv.config()
+const poolConfig : PoolConfig = {
+    host: "localhost",
+    port: 5432,
+    database: "db_typescript",
+    user: "postgres",
+    password: "080596",
+};
+
+interface options{
+    filter_and: Array<string>,
+}
 export class Postegres{
     private pool : Pool;
     constructor(){
@@ -8,9 +20,55 @@ export class Postegres{
     }
 
     // métodos para conecção com banco de dados
-    select(table : string, columns: Array<string> = [], options: object = {} ){
-        const response = {'nome': 'fulano', 'password': '1245', 'email': 'ful@g.com'} // expemplo de resposta da query
-        return {err: null, data: response}; // objeto com erro ou resposta
+    public async select(table : string, columns: Array<string> = ['*'], options: options | undefined ){
+        // {
+        //     filter_and: []
+        // }
+        try{
+            const values = [];
+            const columnSize = columns.length;
+            let dolarColumns : any | string = []; // [$1, $2]
+            for (let i = 0; i < columnSize; i++){
+                dolarColumns.push(`$${i + 1} `);
+            }
+            dolarColumns = dolarColumns.toString();
+            // dolarColumns = $1, $2
+            values.push(... columns);
+            //values ['username', 'email']
+            // values.push(table);
+            const queryText = `
+                SELECT ${dolarColumns}
+                FROM ${table}
+            `
+            const qr = {
+                text: queryText,
+                values: values
+            }
+            console.log(queryText);
+            console.log(values);
+
+
+            const dbRes = await this.pool.query(qr);
+            return {err: null, data: dbRes};
+        }catch(err){
+            return err;
+        }
+        // return {err: null, data: response}; // objeto com erro ou resposta
+    };
+
+    public insert(){
+        return;
+    };
+
+    public delete(){
+        return;
+    };
+
+    public update(){
+        return;
     };
 
 }
+
+const orm = new Postegres;
+orm.select('usuario', ["username", "email"]).then(res => console.log(res));
