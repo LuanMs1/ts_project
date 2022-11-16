7d486e5bf383dfe76e0582ff4f0fe16b53d097b7
yanvictorsn <yanvictosn@gmail.com>
2022-11-16 15:26:32 -0300

    fixing tema services and controller

---
+++ src/services/registerTeam.ts
@@ -0,0 +1,27 @@
+import { v4 as uuid } from "uuid";
+import * as validator from "../validator/validators.js";
+import { registerTeam } from "../interfaces/interfaces.js";
+import { Database } from "../repositories/index.js";
+
+export default function register(teamData: registerTeam) {
+    try {
+        new validator.TeamValidator(teamData.name, "name");
+        new validator.LeaderValidator(teamData.leader, "leader");
+
+        const id = uuid();
+        teamData.id = id;
+
+        // const data = new Database.postUser(userData);
+        // if(data.err !== null) {
+        //     throw {
+        //         status: 500,
+        //         message: "Erro no banco de dados!",
+        //     };
+        // }
+    } catch (error: any) {
+        throw {
+            status: error.status,
+            message: error.message,
+        };
+    }
+}
